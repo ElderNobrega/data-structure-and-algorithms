19ce72ec239a4f0f5b2d0c7f9432eb44d7c1b479
ElderNobrega <eldernguerra.filho@hotmail.com>
2020-07-03 20:12:45 -0400

    updates

--- javaScript/twoSum.js
+++ javaScript/twoSum.js
@@ -4,9 +4,23 @@ Given an array of integers, return indices of the two numbers such that they add
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 */
 
-//solution using 
-var twoSum = function(array, target) {
-    for (let i = 0; i < array.length; i++) {
+/*
+Given nums = [2, 7, 11, 15], target = 9,
 
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+ */
+
+
+//solution using single loop
+var twoSum = function(nums, target) {
+
+    var firstNum = 0
+    var restSum = 0
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < target && restSum == 0) {
+
+        }
     }
 }
