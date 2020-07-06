7941914c5edf6b24b830e48094bcc81edf30a84a
ElderNobrega <eldernguerra.filho@hotmail.com>
2020-07-06 15:37:45 -0400

    Final update

--- javaScript/twoSum.js
+++ javaScript/twoSum.js
@@ -8,19 +8,19 @@ You may assume that each input would have exactly one solution, and you may not 
 Given nums = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
+return [0, 1]
  */
 
 
 //solution using single loop
 var twoSum = function(nums, target) {
-
-    var firstNum = 0
-    var restSum = 0
     
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] < target && restSum == 0) {
-
-        }
+        for (let j = 0 + i; j < nums.length; j++) {
+            if (target == nums[i] + nums[j]) 
+                return [i,j]
+        }    
     }
 }
+
+twoSum([2, 7, 11, 15], 9)
