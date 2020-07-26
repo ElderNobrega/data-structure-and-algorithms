06503adb8305ea314335700a599419eae76fdf12
ElderNobrega <eldernguerra.filho@hotmail.com>
2020-07-26 11:27:34 -0400

    Linked List

---
+++ Linked-List/linkedList.js
@@ -0,0 +1,16 @@
+// 10 -> 5 -> 16
+
+class LinkedList {
+    constructor(value) {
+        this.head = {
+            value: value,
+            next: null
+        }
+        this.tail = this.headt
+        this.length = 1
+    }
+
+    append(value) {
+        
+    }
+}
