9c50912e6cb6940812b272c1422d876c64b3b1db
ElderNobrega <eldernguerra.filho@hotmail.com>
2020-07-29 17:40:58 -0400

    add to the end of doubly linked list

--- Linked-List/doublyLinkedList.js
+++ Linked-List/doublyLinkedList.js
@@ -7,7 +7,18 @@ class doublyLinkedList {
         this.head = new Node(num, this.head)
     }
 
-    
+    addLast(num) {
+        if (this.head == null) {
+            this.head = new Node(num)
+        }
+        else {
+            let curr = this.head
+            while (curr.next != null) {
+                curr = curr.next
+            }
+            curr.next = new Node(num, null, curr)
+        }
+    }
 }
 
 class Node {
@@ -19,4 +30,5 @@ class Node {
 }
 const newLinkedList = new doublyLinkedList()
 newLinkedList.addFront(10)
+newLinkedList.addLast(15)
 console.log(newLinkedList)
