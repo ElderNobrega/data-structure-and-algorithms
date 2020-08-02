6fb31f55411557907101bc4050634ca7aef75ebd
ElderNobrega <eldernguerra.filho@hotmail.com>
2020-08-02 17:59:15 -0400

    Queues completed

--- Queues/queues.js
+++ Queues/queues.js
@@ -12,5 +12,41 @@ class Queue {
         this.length = 0
     }
 
-    
+    peek() {
+        return this.first
+    }
+
+    enqueue(value) {
+        if (this.length == 0) {
+            this.first = new Node(value)
+            this.last = this.first
+        }
+        else {
+            this.last.next = new Node(value)
+            this.last = this.last.next
+        }
+        this.length++
+    }
+
+    dequeue() {
+        if(!this.first) {
+            return null
+        }
+        if (this.first == this.last) {
+            this.last = null
+        }
+            this.first = this.first.next
+            this.length--
+    }
 }
+
+const myQueue = new Queue()
+
+myQueue.enqueue('Maria')
+myQueue.enqueue('Jhon')
+myQueue.enqueue('Jen')
+myQueue.dequeue()
+myQueue.dequeue()
+myQueue.dequeue()
+console.log(myQueue.peek())
+console.log(myQueue)
