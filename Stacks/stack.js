f96e6eea03a3d1bdc89b38adb811676ee9d87c0f
ElderNobrega <eldernguerra.filho@hotmail.com>
2020-08-02 14:29:56 -0400

    Stack completed

--- Stacks/stack.js
+++ Stacks/stack.js
@@ -1,7 +1,7 @@
 class Node {
-    constructor(value) {
+    constructor(value, next=null) {
         this.data = value
-        this.next = null
+        this.next = next
     } 
 }
 
@@ -13,16 +13,57 @@ class Stack {
     }
 
     peek(){
-
+        if (!this.bottom) {
+            return console.log('Your stack is empty')
+        }
+        if (this.top) {
+            return this.top
+        }
     }
 
     push(value) {
-
+        if (!this.bottom) {
+            this.bottom = new Node(value)
+            this.top = this.bottom
+            this.length++
+        }
+        else {
+            this.top.next = new Node(value)
+            this.top = this.top.next
+            this.length++
+        }
     }
 
     pop() {
-
+        if (!this.top) {
+            return null
+        }
+        if (this.bottom === this.top) {
+            this.bottom = null
+            this.top = null
+            this.length--
+        }
+        else {
+            let curr = this.bottom
+            let prev = null
+            while (curr.next != null) {
+                prev = curr
+                curr = curr.next
+            }
+            prev.next = null
+            this.top = prev
+            this.length--
+        }
     }
 }
 
 const myStack = new Stack()
+myStack.push('Google')
+myStack.push('Amazon')
+myStack.push('Youtube')
+console.log(myStack.peek())
+myStack.pop()
+myStack.pop()
+myStack.pop()
+console.log(myStack)
+console.log(myStack.peek())
